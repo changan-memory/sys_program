c4f41560cd6be6d5012518479134411cad17f7b2
changan-memory <qinyuxiangzzu@outlook.com>
2024-11-10 21:41:40 +0800

    update

--- Process_cntl/exec/src/main.cpp
+++ Process_cntl/exec/src/main.cpp
@@ -14,10 +14,15 @@ int main(int argc, char** argv) {
         exit(1);
     } else if (pid == 0) {
         // cout << "i am child process" << endl;
+
         // execlp("ls", "-l", "-d", "-h", NULL);  错误写法 NULL作为哨兵
         // -d 是查看目录，导致出现的结果比较少
         // execlp("ls", "ls", "-l", "-h", "-a", NULL);  // 结尾需要加上 NULL作为哨兵
-        execlp("date", "date", NULL);  // 结尾需要加上 NULL作为哨兵
+        // execlp("date", "date", NULL);  // 结尾需要加上 NULL作为哨兵
+
+        //execl("./test", "./test", NULL);     // 执行自定义的程序
+        execl("/bin/ls", "ls", "-l", NULL);  // 用execl 执行系统自带的程序
+
         cerr << "execlp error" << endl;
         exit(1);
     } else {

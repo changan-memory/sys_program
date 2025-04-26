0e3806433b567ca69619b3fcb278595ced1d0b14
changan-memories <qinyuxiangzzu@outlook.com>
2025-04-26 12:11:30 +0800

    process start_new

--- main_test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(){
-    printf("hello world\n");
-    return 0;
-}
-
--- sys_programme/main.cpp
+++ sys_programme/main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
-
+#include<stdlib.h>
 int main()
 {
     printf("hello wolrd\n");

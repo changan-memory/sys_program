56694e44fb42d07031c0beed9738b188abe6e28a
changan-memories <qinyuxiangzzu@outlook.com>
2025-05-01 14:51:15 +0800

    finish clang-format config

--- basic_command/progress_bar/main.c
+++ basic_command/progress_bar/main.c
@@ -1,16 +1,16 @@
 #include <unistd.h>
+
 #include "progressBar.h"
 
-void test(){
-    //printf("hello world\n");  验证缓冲区的存在
-    //printf("hello world");
-    
+void test() {
+    // printf("hello world\n");  验证缓冲区的存在
+    // printf("hello world");
+
     //实现一个倒计时
     int cnt = 100;
-    while(cnt >= 0){
-
-        //printf("%-2d\r", cnt);//使用\r回车会导致三位数只刷新了两位数
-        printf("%-3d\r", cnt);// %3d\r可以实现在行的开头更新数字  -相当于反转  确保是左对齐
+    while (cnt >= 0) {
+        // printf("%-2d\r", cnt);//使用\r回车会导致三位数只刷新了两位数
+        printf("%-3d\r", cnt);  // %3d\r可以实现在行的开头更新数字  -相当于反转  确保是左对齐
         fflush(stdout);
         cnt--;
         sleep(1);
@@ -20,24 +20,24 @@ void test(){
 
 //通过任务调用进度条
 //外部程序通过回调，调用进度条
-//void (*callback_t)(int) , callback_t是函数指针 typedef void (*)(int) callback_t  把callback_t变成函数指针类型
-//typedef void (*callback_t)(int);    
-void downLoad(callback_t cb){
+// void (*callback_t)(int) , callback_t是函数指针 typedef void (*)(int) callback_t  把callback_t变成函数指针类型
+// typedef void (*callback_t)(int);
+void downLoad(callback_t cb) {
     //模拟调用进度条
     int total = 1000;
-    int curr = 0;     //目前curr需要从0开始
-    while(curr <= total){
+    int curr = 0;  //目前curr需要从0开始
+    while (curr <= total) {
         //进行某种下载任务
         usleep(50000);
         int rate = curr * 100 / total;
 
-        cb(rate);   //回调展示进度
-        curr+=10;
+        cb(rate);  //回调展示进度
+        curr += 10;
     }
-    printf("\n");        //防止命令行提示符影响效果
+    printf("\n");  //防止命令行提示符影响效果
 }
-int main(){   
-    //test();
+int main() {
+    // test();
     printf("downLoan 1:\n");
     downLoad(progressbar);
     initBar();
@@ -47,9 +47,6 @@ int main(){
     printf("downLoan 3:\n");
     downLoad(progressbar);
     initBar();
-    
-
-
 
     //模拟调用进度条
     // int total = 1000, curr = 0;     //目前curr需要从0开始

68c7fd09eed6b3731b8d24255ad643d42d74cb60
changan-memories <qinyuxiangzzu@outlook.com>
2025-05-01 11:51:59 +0800

    finsih progressBar

--- basic_command/progress_bar/main.c
+++ basic_command/progress_bar/main.c
@@ -18,10 +18,49 @@ void test(){
     printf("\n");
 }
 
+//通过任务调用进度条
+//外部程序通过回调，调用进度条
+//void (*callback_t)(int) , callback_t是函数指针 typedef void (*)(int) callback_t  把callback_t变成函数指针类型
+//typedef void (*callback_t)(int);    
+void downLoad(callback_t cb){
+    //模拟调用进度条
+    int total = 1000;
+    int curr = 0;     //目前curr需要从0开始
+    while(curr <= total){
+        //进行某种下载任务
+        usleep(50000);
+        int rate = curr * 100 / total;
 
-
+        cb(rate);   //回调展示进度
+        curr+=10;
+    }
+    printf("\n");        //防止命令行提示符影响效果
+}
 int main(){   
-    //progressbar(); 
-    test();
+    //test();
+    printf("downLoan 1:\n");
+    downLoad(progressbar);
+    initBar();
+    printf("downLoan 2:\n");
+    downLoad(progressbar);
+    initBar();
+    printf("downLoan 3:\n");
+    downLoad(progressbar);
+    initBar();
+    
+
+
+
+    //模拟调用进度条
+    // int total = 1000, curr = 0;     //目前curr需要从0开始
+    // while(curr <= total){
+    //     progressbar(curr * 100 / total);
+    //     //模拟正在进行某种下载任务
+    //     curr += 10;
+    //     usleep(50000);
+    //     //printf("\n");        //防止命令行提示符影响效果
+    // }
+    // printf("\n");        //防止命令行提示符影响效果
+
     return 0;
 }
--- basic_command/progress_bar/progressBar.h
+++ basic_command/progress_bar/progressBar.h
@@ -2,4 +2,15 @@
 
 #include <stdio.h>
 
-extern void progressbar();
+#define NUM 102     //102 表示字符数组的长度 0-100 101个字符  末尾是\n, 因此大小是 102
+#define BODY '='
+#define HEAD '>'
+#define TOP 100
+
+typedef void (*callback_t)(int);    //函数指针类型 
+
+extern void progressbar(int rate);  //extern 声明外部变量时必须加上  函数声明可加可不加
+extern void downLoad(callback_t cb);
+extern void initBar();
+ 
+

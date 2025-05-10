f0f2b1d1febb874f479b81128fa53e94214640b5
changan-memories <qinyuxiangzzu@outlook.com>
2025-05-10 14:23:59 +0800

    process begin

--- sys_programme/process_ctrl/proc.c
+++ sys_programme/process_ctrl/proc.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int main() {
+    pid_t pid = getpid();
+    pid_t ppid = getppid();
     while (1) {
-        printf("hello you\n");
+        printf("I am a precess, my ID is %d, my parent ID is %d\n", pid, ppid);
         sleep(1);
     }
     return 0;

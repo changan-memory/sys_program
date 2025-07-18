ff800a7ad31daf80cf8a68da1bb147cf39975314
changan-memory <qinyuxiangzzu@outlook.com>
2025-07-18 11:51:39 +0800

    learn process status

---
+++ sys_programme/3_status_process/myproc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid = fork();
+    if (pid == 0) {
+        // child
+        int cnt = 5;
+        while (cnt) {
+            printf("I am child, pid: %d, ppid: %d, cnt: %d\n", getpid(), getppid(), cnt);
+            cnt--;
+            sleep(1);
+        }
+        exit(0);
+    } else {
+        // father
+        while (1) {
+            printf("I am father, pid: %d, ppid: %d\n", getpid(), getppid());
+            sleep(1);
+        }
+
+        // 父进程没有针对子进程干任何事情
+    }
+    return 0;
+}
+
+// int main() {
+//     while (1) {
+//         sleep(1);
+//         printf("hello world\n");
+//     }
+//     // int a = 0;
+//     // printf("Please Enter #:");
+//     // scanf("%d", &a);
+//     // printf("%d\n", a);
+
+//     return 0;
+// }

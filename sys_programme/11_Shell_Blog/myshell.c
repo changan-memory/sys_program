fbd0ee98c5f15c92a27a31605cad489e067c89e6
changan-memory <qinyuxiangzzu@outlook.com>
2025-11-30 21:01:00 +0800

    finsih blog myShell part one and two

--- sys_programme/11_Shell_Blog/myshell.c
+++ sys_programme/11_Shell_Blog/myshell.c
@@ -1,141 +1,3 @@
-// #include <assert.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <sys/types.h>
-// #include <sys/wait.h>
-// #include <unistd.h>
-
-// #define LEFT "{"
-// #define RIGHT "}"
-// #define LABEL_ROOT "#"
-// #define LABEL_USER "$"
-
-// #define DELIM " \t"
-
-// #define LINE_SIZE 1024
-// #define SPILT_SIZE 512
-
-// #define EXIT_CODE 66
-
-// #define ARGC_SZIE 32
-
-// char commandLine[LINE_SIZE];
-
-// char* argv[ARGC_SZIE];
-
-// char spiltPath[SPILT_SIZE];
-
-// int lastCode = 0;
-
-// const char* getUserName()  // 获取用户名
-// {
-//     return getenv("USER");
-// }
-
-// const char* getPwd()  // 获取当前路径
-// {
-//     char* curPath = getenv("PWD");
-
-//     int len = strlen(curPath);
-//     int i = len - 1;
-//     while (i >= 0 && curPath[i] != '/')
-//         --i;
-//     // i 停在 倒数第一个 '/' 的位置
-//     strcpy(spiltPath, curPath + i + 1);
-//     return spiltPath;
-// }
-
-// void interate()
-// {
-//     if (strcmp("root", getUserName()) == 0)
-//         printf(LEFT "%s@hcss-ecs-dfa9:~%s" LABEL_ROOT RIGHT " ", getUserName(), getPwd());
-//     else
-//         printf(LEFT "%s@hcss-ecs-dfa9:~%s" LABEL_USER RIGHT " ", getUserName(), getPwd());
-
-//     // scanf("%s", commandLine);
-//     char* s = fgets(commandLine, sizeof(commandLine), stdin);
-
-//     // 输入命令时，会将字符串 最后键入的回车也读入，我们不想读入回车
-//     // strlen(commandLine) - 1  最小值为 0
-//     commandLine[strlen(commandLine) - 1] = '\0';  // 读入的字符串为 "xxxx\n\0"，将\n改为字符串的结束标记\0即可
-
-//     // 以下三行为 debug
-//     assert(s);
-//     (void) s;
-//     // printf("%s", s);  // 我们手动将回车处理掉了
-// }
-
-// int splitString(char* cLine, char* _argv[])
-// {
-//     int i = 0;
-//     // _argv[i++] = strtok(cLine, DELIM);
-//     _argv[i++] = strtok(commandLine, DELIM);
-
-//     while (_argv[i++] = strtok(NULL, DELIM))  // 接着从上次的位置继续分割
-//         ;                                     // 一行代码完成字符串的分割，并且将argv中的最后一个参数置为NULL
-//     return i - 1;
-// }
-
-// void nomalExecute(char* _argv[])
-// {
-//     pid_t id = fork();
-//     if (id < 0)
-//     {
-//         perror("fork failed\n");
-//         return;
-//     }
-//     else if (id == 0)
-//     {
-//         // 子进程执行指令
-//         // execvpe(argv[0], argv, environ);
-
-//         execvp(_argv[0], _argv);
-//         // 程序替换有可能失败  exec 没有成功返回值，只有失败返回值
-//         exit(EXIT_CODE);
-//     }
-//     else
-//     {
-//         // 父进程等待
-//         int status = 0;                          // 获取子进程的退出状态
-//         pid_t retPid = waitpid(id, &status, 0);  // 等待子进程，传入status 阻塞等待
-//         if (retPid == id)
-//         {
-//             // 等待成功的处理逻辑
-//             lastCode = WEXITSTATUS(status);
-//         }
-//     }
-// }
-
-// int main()
-// {
-//     int quit = 0;
-//     while (!quit)
-//     {
-//         // 1. 交互
-//         interate();
-
-//         // 2. 命令行解析
-//         // 读取字符串后，对命令行做解析，切割
-//         // ls -a -l -> "ls" "-a" "-l"
-//         int argc = splitString(commandLine, argv);
-//         if (argc == 0)
-//             continue;
-
-//         // 3. 优先判断是否是内建命令
-
-//         // 4. 执行普通命令
-//         nomalExecute(argv);
-
-//         // debug
-//         for (int i = 0; argv[i]; ++i)
-//         {
-//             printf("[%d]-> : %s\n", i, argv[i]);
-//         }
-//     }
-//     return 0;
-// }
-
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -369,31 +231,23 @@ int buildExecute(int _argc, char* _argv[])
 
 int main()
 {
-    char* argv[ARGC_SIZE];
-    // shell 本质是一个死循环
-
     while (!quit)
     {
         // 1. 输出命令行提示符号进行交互，并读取输入的命令字符串
-        interact(commandLine, sizeof(commandLine));
+        getPwd();
+        if (strcmp(getUserName(), "root") == 0)
+            printf(LEFT "%s@hcss-ecs-dfa9:%s" RIGHT LABEL_ROOT, getUserName(), pwd);
+        else
+            printf(LEFT "%s@hcss-ecs-dfa9:%s" RIGHT LABEL_USER, getUserName(), pwd);
 
-        // 2. 对输入的命令进行命令行解析，，拆分为 指令名 + 选项
-        int argc = splitString(commandLine, argv, ARGC_SIZE);
+        char* str = fgets(commandLine, sizeof(commandLine), stdin);
+        assert(str);  // 即使直接输入回车，也有一个字符，因此str不可能为空
 
-        // 3. 判断输入的指令是否有效，无效时什么都不做
-        if (argc == 0)
-            continue;
+        // 下标 strlen(commandLine) 指向的为 '\0'，再减一定位到 末尾的换行符
+        commandLine[strlen(commandLine) - 1] = '\0';
 
-        // 验证解析出来的字符串
-        // for (int i = 0; argv[i]; ++i)
-        //     printf("[%d]->: %s\n", i, argv[i]);
-
-        // 4. 判断是否是内建命令，内建命令调用Shell 内部的函数执行
-        int isBuild = buildExecute(argc, argv);
-
-        // 5. 非内建命令，fork 出子进程, 执行命令
-        if (!isBuild)
-            nomalExecute(argv);
+        // 验证结果，看读入的字符串是否正确
+        printf("test:: %s", str);
     }
     return 0;
 }
